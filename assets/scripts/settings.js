b564f0c53817104d0496c654019b5fa85c70ecea
Raphael Neko <contact@raphaelneko.com>
2025-04-23 02:03:45 +0200

    Welcome text added

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -1,9 +1,15 @@
 const settingsMenu = document.getElementById('settings-modal');
+const settingWelcomeText = document.getElementById('setting-welcome-text');
 const settingSpoiler = document.getElementById('setting-spoilers');
 const settingDarkTheme = document.getElementById('setting-dark-theme');
 
 function toggleSettingsVisibility() {
 	settingsMenu.classList.toggle('opened');
+}
+
+function toggleWelcomeText() {
+	settingWelcomeText.classList.toggle('enabled');
+	document.body.classList.toggle('hide-welcome');
 }
 
 function toggleSpoilers() {
